32cb0ef0f56cad183438ad3bdfacfc92829639ee
Mike Newman <MikeGTN@src.gnome.org>
2003-08-22 08:30:15 +0000

    Fixed i18n of help messages.

--- src/main.c
+++ src/main.c
@@ -1012,6 +1012,7 @@ main (gint argc, gchar **argv) {
   poptContext ctx;
   gint nextopt, retval;
 
+  setlocale(LC_ALL,"");
   bindtextdomain(GETTEXT_PACKAGE, GNOMELOCALEDIR);
   bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
   textdomain(GETTEXT_PACKAGE);
