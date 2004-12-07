69e094a4570bae10dab378e8c024c76fc7d0250a
Glynn Foster <glynn.foster@sun.com>
2004-12-07 01:26:00 +0000

    Fix for #137993. There is a chance that we'll have to revert this fix,
    2004-12-07  Glynn Foster  <glynn.foster@sun.com>
    
    	* src/main.c: Fix for #137993. There is a chance that we'll have
    	to revert this fix, given the comments in /etc/X11/gdm/Xsession:
    
            # Note that this should only go to zenity dialogs which always
    	#  expect utf8
            gettextfunc () {
              if [ "x$gdmtranslate" != "x" ] ; then
                "$gdmtranslate" --utf8 "$1"
              else
                echo "$1"
              fi
            }
    
    	So I guess we may be over a barrel with our original guarantee.
    	Let's just change this in 2.9.x and see if anyone notices or cares
            enough. Patch from Leonardo Boshell <p@kapcoweb.com>.

--- src/main.c
+++ src/main.c
@@ -44,11 +44,11 @@ main (gint argc, gchar **argv) {
 #endif
 
   bindtextdomain(GETTEXT_PACKAGE, GNOMELOCALEDIR);
-  bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
   textdomain(GETTEXT_PACKAGE);
 
   results = zenity_option_parse (argc, argv);
 
+  bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
   gtk_init (&argc, &argv);
 
   switch (results->mode) {
