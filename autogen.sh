59b5bad79402b81b130e3b4242a74611d1c3be63
Lucas Rocha <lucasr@gnome.org>
2006-05-10 03:37:14 +0000

    move to automake 1.9 and several cleanups (Fixes bug #341056). Patch from
    2006-05-10  Lucas Rocha  <lucasr@gnome.org>
    
    	* .cvsignore, Makefile.am, autogen.sh, configure.in,
    	data/Makefile.am, po/.cvsignore, src/Makefile.am: move to
    	automake 1.9 and several cleanups (Fixes bug #341056).
    	Patch from Christian Persch <chpe@gnome.org>.

--- autogen.sh
+++ autogen.sh
@@ -5,7 +5,7 @@ srcdir=`dirname $0`
 test -z "$srcdir" && srcdir=.
 
 PKG_NAME="zenity"
-REQUIRED_AUTOMAKE_VERSION=1.7
+REQUIRED_AUTOMAKE_VERSION=1.9
 
 (test -f $srcdir/configure.in \
   && test -f $srcdir/ChangeLog \
@@ -20,4 +20,4 @@ which gnome-autogen.sh || {
     echo "You need to install gnome-common from the GNOME CVS"
     exit 1
 }
-USE_GNOME2_MACROS=1 USE_COMMON_DOC_BUILD=yes . gnome-autogen.sh
+USE_GNOME2_MACROS=1 . gnome-autogen.sh
