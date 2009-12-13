97d7fa636e4f5ee20a53b667d9d27303b951a05d
Lucas Rocha <lucasr@gnome.org>
2009-12-13 17:45:28 +0000

    Bug 603673 - Show correct license in the about dialog

--- src/about.c
+++ src/about.c
@@ -62,14 +62,14 @@ static gchar *translators;
 
 static const char *license[] = {
 	N_("This program is free software; you can redistribute it and/or modify "
-	   "it under the terms of the GNU General Public License as published by "
+	   "it under the terms of the GNU Lesser General Public License as published by "
 	   "the Free Software Foundation; either version 2 of the License, or "
 	   "(at your option) any later version.\n"),
 	N_("This program is distributed in the hope that it will be useful, "
 	   "but WITHOUT ANY WARRANTY; without even the implied warranty of "
 	   "MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the "
 	   "GNU General Public License for more details.\n"),
-	N_("You should have received a copy of the GNU General Public License "
+	N_("You should have received a copy of the GNU Lesser General Public License "
 	   "along with this program; if not, write to the Free Software "
 	   "Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.")
 };
