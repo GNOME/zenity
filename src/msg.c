f77a0199af3a3d1362be95ab651186546492e8cd
Arx Cruz <arxcruz@src.gnome.org>
2011-06-16 14:45:52 -0300

    Fix the msg forms (question, warning, error) to return 5 when timeout

--- src/msg.c
+++ src/msg.c
@@ -138,7 +138,7 @@ zenity_msg (ZenityData *data, ZenityMsgData *msg_data)
   zenity_util_show_dialog (dialog);
 
   if(data->timeout_delay > 0) {
-    g_timeout_add_seconds (data->timeout_delay, (GSourceFunc) zenity_util_timeout_handle, dialog);
+    g_timeout_add_seconds (data->timeout_delay, (GSourceFunc) zenity_util_timeout_handle, NULL);
   }
 
   g_object_unref (builder);
